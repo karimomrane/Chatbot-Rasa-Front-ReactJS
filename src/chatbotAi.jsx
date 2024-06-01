ca37b14b20981c05f9e52b32e1a187de70e9fb74
Karim Omrane <KOMRANE@Benyaghlane.tn>
2024-06-02 00:55:21 +0100

    BY Style

--- src/chatbotAi.jsx
+++ src/chatbotAi.jsx
@@ -1,5 +1,5 @@
 import './chatBot.css';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import { IoMdSend } from 'react-icons/io';
 import { BiBot, BiUser } from 'react-icons/bi';
 import { BsFillMicFill } from 'react-icons/bs';
@@ -8,10 +8,17 @@ function ChatbotAi() {
     const [chat, setChat] = useState([]);
     const [inputMessage, setInputMessage] = useState('');
     const [botTyping, setBotTyping] = useState(false);
+    const [showMoreOptions, setShowMoreOptions] = useState(false);
+
+
+    const handleMoreClick = () => {
+        setShowMoreOptions(!showMoreOptions);
+    };
+
 
     useEffect(() => {
         const objDiv = document.getElementById('messageArea');
-        objDiv.scrollTop = objDiv.scrollHeight;
+        objDiv.scrollTo({ top: objDiv.scrollHeight, behavior: 'smooth' });
     }, [chat]);
 
     useEffect(() => {
@@ -21,12 +28,12 @@ function ChatbotAi() {
     const sendWelcomeMessage = () => {
         const welcomeMessage = {
             sender: "bot",
-            msg: "Bonjour, je suis EspritNetworkBot. Comment puis-je vous aider ?"
+            msg: "Bonjour, je suis ByBot. Comment puis-je vous aider ?"
         };
         setChat([welcomeMessage]);
     }
 
-    const handleSubmit = (evt) => {
+    const handleSubmit = useCallback((evt) => {
         evt.preventDefault();
         const name = "montasser";
         const request_temp = { sender: "user", sender_id: name, msg: inputMessage.trim() };
@@ -39,49 +46,81 @@ function ChatbotAi() {
         } else {
             window.alert("Veuillez entrer un message valide");
         }
-    }
+    }, [inputMessage]);
 
     const rasaAPI = async (name, msg) => {
-        await fetch('http://localhost:5005/webhooks/rest/webhook', {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                'charset': 'UTF-8',
-            },
-            credentials: "same-origin",
-            body: JSON.stringify({ "sender": name, "message": msg }),
-        })
-            .then(response => response.json())
-            .then((response) => {
-                let botResponse = ''; // Initialize an empty string to store bot responses
-                if (response && response.length > 0) {
-                    botResponse = response.map(item => item.text).join('<br/>'); // Concatenate all bot responses into one string with newline characters
-                    const message = {
-                        sender: "bot",
-                        msg: botResponse
-                    };
-                    setChat(chat => [...chat, message]);
-                }
-                setBotTyping(false);
+        try {
+            const response = await fetch('http://localhost:5005/webhooks/rest/webhook', {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'charset': 'UTF-8',
+                },
+                credentials: "same-origin",
+                body: JSON.stringify({ "sender": name, "message": msg }),
             });
+            const data = await response.json();
+            let botResponse = '';
+            if (data && data.length > 0) {
+                botResponse = data.map(item => item.text).join('<br/>');
+                const message = {
+                    sender: "bot",
+                    msg: botResponse
+                };
+                setChat(chat => [...chat, message]);
+            }
+            setBotTyping(false);
+        } catch (error) {
+            console.error('Error:', error);
+            setBotTyping(false);
+        }
     }
 
-
-    const handleQuestionClick = (question) => {
+    const handleQuestionClick = useCallback((question) => {
         const name = "montasser";
         setChat(chat => [...chat, { sender: "user", sender_id: name, msg: question }]);
         setBotTyping(true);
         rasaAPI(name, question);
-    }
+        setShowMoreOptions(false)
+    }, []);
 
     return (
         <div style={{ marginTop: "100px" }}>
+            {showMoreOptions && (
+                <>
+                    <div className="modal" style={{ padding: "30px", position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', zIndex: 999, backgroundColor: '#ffffff' }}>
+                        <div className="modal-dialog modal-dialog-centered" role="document">
+                            <div className="modal-content">
+                                <div className="modal-header">
+                                    <h2 className="modal-title text-center">More Options</h2>
+                                    <button type="button" className="close" onClick={handleMoreClick} style={{ position: 'absolute', right: '15px', top: '15px', border: '1px solid black' }}>
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div className="modal-body">
+                                    <button className="btn btn-outline-light mx-2 my-2" onClick={() => handleQuestionClick("Another question 1")}>Another question 1</button>
+                                    <button className="btn btn-outline-light mx-2 my-2" onClick={() => handleQuestionClick("Another question 2")}>Another question 2</button>
+                                    <button className="btn btn-outline-light mx-2 my-2" onClick={() => handleQuestionClick("Another question 3")}>Another question 3</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="chatbot-blur"></div>
+                </>
+            )}
+
+
+
+
+
+
+
             <div className="container">
                 <div className="row justify-content-center">
                     <div className="card stylecard">
                         <div className="cardHeader styleHeader">
-                            <h3>Assistant Esprit Network</h3>
+                            <h3>Assistant Ben Yaghlane Shops</h3>
                         </div>
                         <div className="cardBody styleBody" id="messageArea">
                             <div className="row msgarea">
@@ -90,7 +129,8 @@ function ChatbotAi() {
                                         {user.sender === 'bot' ?
                                             (
                                                 <div className='msgalignstart'>
-                                                    <BiBot className="botIcon" /><h6 className='botmsg' dangerouslySetInnerHTML={{ __html: user.msg }} />
+                                                    <img src="/src/assets/yaghlane.jpg" alt="bot" className="botImage" />
+                                                    <h6 className='botmsg' dangerouslySetInnerHTML={{ __html: user.msg }} />
                                                 </div>
                                             )
                                             : (
@@ -104,11 +144,15 @@ function ChatbotAi() {
                                 {botTyping ? <h6>Bot saisie....</h6> : null}
                             </div>
                         </div>
-                        <div className="col-12 mx-4 my-2">
-                            <button className="btn btn-outline-dark mx-2" onClick={() => handleQuestionClick("Comment créer une offre ?")}>Comment créer une offre ?</button>
-                            <button className="btn btn-outline-dark mx-2 my-1" onClick={() => handleQuestionClick("Comment lister mes offres ?")}>Comment lister mes offres ?</button>
-                            <button className="btn btn-outline-dark mx-2 my-1" onClick={() => handleQuestionClick("Comment lister mes archive ?")}>Comment lister mes archives des offres ?</button>
+
+                        <div className="col-12 mx-4" style={{ marginBottom: "20px", marginLeft: "20px" }}>
+                            <button className="btn btn-outline-light mx-2" onClick={() => handleQuestionClick("Comment créer une offre ?")}>Comment créer une offre ?</button>
+                            <button className="btn btn-outline-light mx-2 my-2" onClick={() => handleQuestionClick("Comment lister mes offres ?")}>Comment lister mes offres ?</button>
+                            <button className="btn btn-outline-light mx-2 my-2" onClick={() => handleQuestionClick("Comment lister mes archive ?")}>Comment lister mes archives des offres ?</button>
+                            <button className="btn btn-outline-light mx-2 my-2" onClick={handleMoreClick}>More...</button>
                         </div>
+
+
                         <div className="cardFooter styleFooter">
                             <div className="row">
                                 <form style={{ display: 'flex', width: '100%' }} onSubmit={handleSubmit}>
@@ -116,7 +160,7 @@ function ChatbotAi() {
                                         <input onChange={e => setInputMessage(e.target.value)} value={inputMessage} placeholder='Posez votre question ici' type="text" className="msginp" />
                                     </div>
                                     <div className="col-2 cola">
-                                        <button type="submit" className="circleBtn"><IoMdSend className="sendBtn" /></button>
+                                        <button type="submit" className="circleBtn" aria-label="Send message"><IoMdSend className="sendBtn" /></button>
                                     </div>
                                 </form>
                             </div>
